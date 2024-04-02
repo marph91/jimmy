665b8803f06677ff08cda61d5d8e1fcd2e45bec3
Martin Dörfelt <martin.d@andix.de>
2024-04-02 18:51:23 +0200

    separate into testable function

--- src/joplin_custom_importer.py
+++ src/joplin_custom_importer.py
@@ -45,6 +45,29 @@ def convert_file(file_: Path, parent: Notebook) -> Tuple[Notebook, list]:
     return parent, []
 
 
+def convert_all_inputs(inputs, app):
+    # parent notebook
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    parent = Notebook({"title": f"{now} - Import"})
+    all_tags = []
+    for single_input in inputs:
+        # Convert the input data to an intermediate representation
+        # that can be used by the importer later.
+        # Try to use an app specific converter. If there is none,
+        # fall back to the default converter.
+        try:
+            module = importlib.import_module(f"apps.{app}")
+            conversion_function = module.convert
+        except ModuleNotFoundError:
+            conversion_function = (
+                convert_file if single_input.is_file() else convert_folder
+            )
+        # TODO: children are added to the parent node / node tree implicitly
+        note_tree, tags = conversion_function(single_input, parent)
+        all_tags.extend(tags)
+    return note_tree, all_tags
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -65,25 +88,7 @@ def main():
         # create the connection to Joplin first to fail fast in case of a problem
         api = api_helper.get_api()
 
-    # parent notebook
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    parent = Notebook({"title": f"{now} - Import"})
-    all_tags = []
-    for single_input in args.input:
-        # Convert the input data to an intermediate representation
-        # that can be used by the importer later.
-        # Try to use an app specific converter. If there is none,
-        # fall back to the default converter.
-        try:
-            module = importlib.import_module(f"apps.{args.app}")
-            conversion_function = module.convert
-        except ModuleNotFoundError:
-            conversion_function = (
-                convert_file if single_input.is_file() else convert_folder
-            )
-        # TODO: children are added to the parent node / node tree implicitly
-        note_tree, tags = conversion_function(single_input, parent)
-        all_tags.append(tags)
+    note_tree, tags = convert_all_inputs(args.input, args.app)
 
     if not args.dry_run:
         # import to Joplin
