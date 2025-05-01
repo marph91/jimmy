52341da69224e380d4b755f8a9f6c69559a4ad49
Martin Dörfelt <martin.d@andix.de>
2025-05-01 14:51:37 +0200

    bear: parse metadata from "info.json"

--- jimmy/formats/textbundle.py
+++ jimmy/formats/textbundle.py
@@ -1,6 +1,7 @@
 """Convert textbundle or textpack notes to the intermediate format."""
 
 import itertools
+import json
 from pathlib import Path
 from urllib.parse import unquote
 
@@ -28,7 +29,11 @@ class Converter(converter.BaseConverter):
         return resources
 
     @common.catch_all_exceptions
-    def convert_note(self, file_: Path, parent_notebook: imf.Notebook):
+    def convert_note(self, file_: Path, parent_notebook: imf.Notebook, metadata: dict):
+        if file_.name == "info.json":
+            return  # handled already
+        if file_.name == "assets":
+            return  # not needed, since it's included in link paths
         if file_.suffix.lower() not in (".md", ".markdown"):
             # take only the exports in markdown format
             self.logger.debug(f'Ignoring folder or file "{file_.name}"')
@@ -49,7 +54,22 @@ class Converter(converter.BaseConverter):
             for tag in jimmy.md_lib.common.get_inline_tags(note_imf.body, ["#"])
         ]
         note_imf.resources = self.handle_markdown_links(note_imf.body)
-        note_imf.time_from_file(file_)
+
+        # handle bear specific metadata
+        if (bear_metadata := metadata.get("net.shinyfrog.bear")) is not None:
+            note_imf.created = bear_metadata.get("creationDate")
+            note_imf.updated = bear_metadata.get("modificationDate")
+            # ID renamed in v2?
+            note_imf.original_id = bear_metadata.get(
+                "uniqueIdentifier"
+            ) or bear_metadata.get("bear-note-unique-identifier")
+
+            for key in ("pinned", "trashed", "archived"):
+                if bool(int(bear_metadata.get(key, False))):
+                    note_imf.tags.append(imf.Tag(f"bear-{key}"))
+
+        if note_imf.created is None and note_imf.updated is None:
+            note_imf.time_from_file(file_)
 
         parent_notebook.child_notes.append(note_imf)
 
@@ -58,8 +78,11 @@ class Converter(converter.BaseConverter):
 
         # We can't check for "is_file()", since ".textbundle" is a folder.
         if file_or_folder.suffix in self.accepted_extensions:
+            metadata = json.loads(
+                (self.root_path / "info.json").read_text(encoding="utf-8")
+            )
             for file_ in sorted(self.root_path.iterdir()):
-                self.convert_note(file_, self.root_notebook)
+                self.convert_note(file_, self.root_notebook, metadata)
         else:
             for file_ in sorted(
                 itertools.chain(
@@ -70,5 +93,9 @@ class Converter(converter.BaseConverter):
                 self.root_path = self.prepare_input(file_)
                 parent_notebook = imf.Notebook(file_.stem)
                 self.root_notebook.child_notebooks.append(parent_notebook)
+
+                metadata = json.loads(
+                    (self.root_path / "info.json").read_text(encoding="utf-8")
+                )
                 for file_ in sorted(self.root_path.iterdir()):
-                    self.convert_note(file_, parent_notebook)
+                    self.convert_note(file_, parent_notebook, metadata)
