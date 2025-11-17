09b17ea6c78e80e033473851967460fc93e00e11
Martin Dörfelt <33229141+marph91@users.noreply.github.com>
2025-11-17 18:21:00 +0100

    prepare for release 1.1.10

--- jimmy/variables.py
+++ jimmy/variables.py
@@ -1,7 +1,7 @@
 """Contains global variables."""
 
 # current version of jimmy
-VERSION = "1.1.9"
+VERSION = "1.1.10"
 
 # all available formats and their allowed inputs
 # use a registry to avoid importing all (including many unneeded) modules
