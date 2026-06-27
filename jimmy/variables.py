7885d5c3cc3fb93297b4c27def87b87d7dc38fa7
Martin Dörfelt <33229141+marph91@users.noreply.github.com>
2026-06-27 10:24:42 +0200

    prepare release v1.1.25

--- jimmy/variables.py
+++ jimmy/variables.py
@@ -4,7 +4,7 @@
 import json
 
 # current version of jimmy
-VERSION = "1.1.24"
+VERSION = "1.1.25"
 
 # all available formats and their allowed inputs
 # use a registry to avoid importing all (including many unneeded) modules
